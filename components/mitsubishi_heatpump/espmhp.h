ece589ee13abeb7cb4ed68c5cf4e090b48fc4026
Alex Van de Putte <alex@vndptt.com>
2023-09-25 13:30:33 -0400

    Update espmhp.h

--- components/mitsubishi_heatpump/espmhp.h
+++ components/mitsubishi_heatpump/espmhp.h
@@ -41,7 +41,7 @@ static const uint8_t ESPMHP_MAX_TEMPERATURE = 31; // degrees C,
 static const float   ESPMHP_TEMPERATURE_STEP = 0.5; // temperature setting step,
                                                     // in degrees C
 
-class MitsubishiHeatPump : public esphome::PollingComponent, public esphome::climate::Climate, public public esphome::Sensor {
+class MitsubishiHeatPump : public esphome::PollingComponent, public esphome::climate::Climate, public esphome::Sensor {
 
     public:
 
